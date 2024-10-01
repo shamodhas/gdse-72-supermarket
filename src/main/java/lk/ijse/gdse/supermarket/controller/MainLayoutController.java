cc3abbdcee874b5dbc09bc477e4309904164159d
shamodhasahan <shamodhasrathnamalala@gmail.com>
2024-10-01 14:43:02 +0530

    initial commit

--- src/main/java/lk/ijse/gdse/supermarket/controller/MainLayoutController.java
+++ src/main/java/lk/ijse/gdse/supermarket/controller/MainLayoutController.java
@@ -14,26 +14,49 @@ package lk.ijse.gdse.supermarket.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 
-public class MainLayoutController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainLayoutController implements Initializable {
 
     @FXML
     private AnchorPane content;
 
-    @FXML
-    void navigateToCustomerPage(ActionEvent event) {
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        navigateTo("/view/CustomerView.fxml");
+    }
+
+    @FXML
+    void navigateToCustomerPage(ActionEvent event)  {
+        navigateTo("/view/CustomerView.fxml");
     }
 
     @FXML
     void navigateToItemPage(ActionEvent event) {
-
+        navigateTo("/view/ItemView.fxml");
     }
 
     @FXML
     void navigateToOrdersPage(ActionEvent event) {
+        navigateTo("/view/OrdersView.fxml");
+    }
 
+    public void navigateTo(String fxmlPath) {
+        try {
+            content.getChildren().clear();
+            AnchorPane load = FXMLLoader.load(getClass().getResource(fxmlPath));
+            content.getChildren().add(load);
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Fail to load page!");
+        }
     }
 
 }
